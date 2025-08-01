086ddeb36aea14433557b44027a79b44c50c6ae1
Raúl Durán <rauldurangarcia2001@gmail.com>
2025-08-01 16:33:44 +0200

    Plugins: Added Telescope.nvim (the fuzzy finder). Link: https://github.com/nvim-telescope/telescope.nvim.git

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,11 @@
+-- Include Telescope (https://github.com/nvim-telescope/telescope.nvim.git).
+return {
+  {
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.8',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+  },
+}
+
