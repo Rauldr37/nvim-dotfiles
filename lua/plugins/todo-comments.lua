d8e3b6b0d95fa8a3bebea8a3ae91836ad906004c
Raúl Durán <rauldurangarcia2001@gmail.com>
2025-08-01 16:45:00 +0200

    Plugins: Added TODO-comments. Link: https://github.com/folke/todo-comments.nvim.git

---
+++ lua/plugins/todo-comments.lua
@@ -0,0 +1,13 @@
+-- Include Folke's todo-comments.nvim (https://github.com/folke/todo-comments.nvim.git).
+return {
+  {
+    'folke/todo-comments.nvim',
+    dependencies = {
+      'nvim-lua/plenary.nvim', 
+    },
+    opts = {
+      -- Use default settings.
+    },
+ },
+}
+
