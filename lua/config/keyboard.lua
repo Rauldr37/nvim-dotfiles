77f48e968c9a070491a9d3d3bdf7b52a28e7c4f2
Raúl Durán <rauldurangarcia2001@gmail.com>
2025-08-01 16:17:09 +0200

    Keybinds: Added Fugitive keybindigs. - Leader+gs for `git status. - Leader+gc for `git commit`. - Leader+ga for `git add`. - Leader+gd for `git diff`. - Leader+gds for `git diff --staged`. - Leader+gpl for `git pull`. - Leader+gps for `git push`.

--- lua/config/keyboard.lua
+++ lua/config/keyboard.lua
@@ -3,3 +3,12 @@
 -- Leader key (spacebar).
 vim.g.mapleader = " "
 
+-- Fugitive keybindings.
+vim.keymap.set('n', '<leader>gs', ':Git status<CR>', { desc = 'Call `git status`' })
+vim.keymap.set('n', '<leader>gc', ':Git commit<CR>', { desc = 'Call `git commit`' })
+vim.keymap.set('n', '<leader>ga', ':Git add ', { desc = 'Prepare the command line to call `git add `' })
+vim.keymap.set('n', '<leader>gd', ':Git diff<CR>', { desc = 'Call `git diff` in a new split' })
+vim.keymap.set('n', '<leader>gds', ':Git diff --staged<CR>', { desc = 'Call `git diff --staged` in a new split' })
+vim.keymap.set('n', '<leader>gps', ':Git push<CR>', { desc = 'Call `git push`' })
+vim.keymap.set('n', '<leader>gpl', ':Git pull<CR>', { desc = 'Call `git pull`' })
+
